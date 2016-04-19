6c22e9ad4fbc48651e3a0147885916f8412e35e9
Reza <reza.bakhshi@accenture.com>
2016-04-19 14:24:59 +0930

    Check if Mash is defined

--- lib/sequel_paperclip.rb
+++ lib/sequel_paperclip.rb
@@ -202,7 +202,7 @@ module Paperclip
       define_method "#{name}=" do |file|
         file = nil if file.is_a?(String) && file == ''
         # Transform Merb Mash to something what paperclip will understant
-        file = MashFile.new(file) if file.is_a?(Mash)
+        file = MashFile.new(file) if defined?(Mash) && file.is_a?(Mash)
         attachment_for(name).assign(file)
       end
 
